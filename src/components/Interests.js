3ac70a58344833c67f24ac6164901bc60c08ae59
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2023-08-28 20:07:48 -0600

    Update styles for mobile devices

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -7,7 +7,7 @@ export default function Interests() {
         <section className="resume-section" id="interests">
             <main>
                 <div className="resume-section-content">
-                    <h2 className="mb-5">Interests</h2>
+                    <h2 className="mb-3">Interests</h2>
                     <p>When I'm not working or studying, I have several hobbies that keep me very busy. 
                         I play guitar, bass, drums, and piano, and enjoy listening to all kinds of music. I  enjoy collecting vinyl records, and currently, my favorite record to spin is <em>Mud Slide Slim and the Blue Horizon</em> by James Taylor. I love to be outside as much as possible, whether I am hiking, biking, playing basketball, or just catching some sunshine! </p>
                     <p className="mb-0"></p>
