5834440bf2ff3b62e25ff74a8970cc55f1987ffd
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2023-08-31 19:51:24 -0600

    Fix spacing in Experience.

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -5,11 +5,11 @@ export default function Experience() {
     <div>
       <section className="resume-section" id="experience">
         <div className="resume-section-content">
-          <h2 className="mb-4">Experience</h2>
-          <div className="d-flex flex-column flex-md-row justify-content-between mb-3">
+          <h2 className="mb-3">Experience</h2>
+          <div className="d-flex flex-column flex-md-row justify-content-between mb-2">
             <div className="flex-grow-1">
               <h3 className="mb-0">Software Developer Intern</h3>
-              <div className="subheading mb-3">Progressive Insurance</div>
+              <div className="subheading mb-2">Progressive Insurance</div>
               <p>
                 Designed and developed an Angular web application for a $1.5 billion subset of customers that saves approximately 302 hours of labor each year.
               </p>
@@ -21,10 +21,10 @@ export default function Experience() {
               <span className="date-range-heading">May 2023 - August 2023</span>
             </div>
           </div>
-          <div className="d-flex flex-column flex-md-row justify-content-between mb-3">
+          <div className="d-flex flex-column flex-md-row justify-content-between mb-2">
             <div className="flex-grow-1">
               <h3 className="mb-0">Web Developer</h3>
-              <div className="subheading mb-3">
+              <div className="subheading mb-2">
                 Colorado State University Honors Program
               </div>
               <p>
@@ -43,11 +43,11 @@ export default function Experience() {
               </span>
             </div>
           </div>
-          <div className="d-flex flex-column flex-md-row justify-content-between mb-3">
+          <div className="d-flex flex-column flex-md-row justify-content-between mb-2">
             <div className="flex-grow-1">
               <h3 className="mb-0">Research Assistant</h3>
-              <div className="subheading mb-3">
-                Colorado State University Computer Science Department
+              <div className="subheading mb-2">
+                CSU Computer Science Department
               </div>
               <p>
                 Researched security threats in Android health and fitness apps using machine learning and natural language processing tools.
