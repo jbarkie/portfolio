a5176b4eaef60d8856845c94ac7461b69d8d9b09
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2024-02-27 21:57:42 -0700

    Refactor Education component
    Remove code smells and duplicate code so that functions are less than 25 lines.

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,44 +1,33 @@
 import React from 'react';
+import schools from '../models/schools';
 
 export default function Education() {
     return (
         <div>
-        <section className="resume-section" id="education">
-            <div className="resume-section-content">
-            <h2 className="mb-4">Education</h2>
-            <div className="d-flex flex-column flex-md-row justify-content-between mb-3">
-                <div className="flex-grow-1">
-                <h3 className="mb-0">Colorado State University</h3>
-                <div className="subheading mb-2">Bachelor of Science - Computer Science</div>
-                <div className="date-range-mobile"><p>August 2021 - December 2023</p></div>
-                <p>GPA: 3.94</p>
-                <p>Graduated cum laude and made Dean's List every semester.</p>
+            <section className="resume-section" id="education">
+                <div className="resume-section-content">
+                    <h2 className="mb-4">Education</h2>
+                    {schools.map((details, index) => <School key={index} {...details} />)}
                 </div>
-                <div className="flex-shrink-0"><span className="date-range-heading">August 2021 - December 2023</span></div>
+            </section>
+            <hr className="m-0" />
+        </div>
+    );
+};
+
+function School({ school, degree, date, gpa, honor }) {
+    return (
+        <div className="d-flex flex-column flex-md-row justify-content-between mb-3">
+            <div className="flex-grow-1">
+                <h3 className="mb-0">{school}</h3>
+                <div className="subheading mb-2">{degree}</div>
+                <div className="date-range-mobile"><p>{date}</p></div>
+                <p>GPA: {gpa}</p>
+                <p>{honor}</p>
             </div>
-            <div className="d-flex flex-column flex-md-row justify-content-between mb-3">
-                <div className="flex-grow-1">
-                <h3 className="mb-0">Front Range Community College</h3>
-                <div className="subheading mb-2">Associate of the Arts</div>
-                <div className="date-range-mobile"><p>August 2018 - May 2020</p></div>
-                <p>GPA: 3.0</p>
-                <p>Earned a Certificate of Business Specialization.</p>
-                </div>
-                <div className="flex-shrink-0"><span className="date-range-heading">August 2018 - May 2020</span></div>
+            <div className="flex-shrink-0">
+                <span className="date-range-heading">{date}</span>
             </div>
-            <div className="d-flex flex-column flex-md-row justify-content-between mb-3">
-                <div className="flex-grow-1">
-                <h3 className="mb-0">Haddam-Killingworth High School</h3>
-                <div className="subheading mb-2">High School Diploma</div>
-                <div className="date-range-mobile"><p>August 2014 - June 2018</p></div>
-                <p>GPA: 4.13</p>
-                <p>Graduated 9th in my class of 155.</p>
-                </div>
-                <div className="flex-shrink-0"><span className="date-range-heading">August 2014 - June 2018</span></div>
-            </div>
-            </div>
-        </section>
-        <hr className="m-0" />
         </div>
     );
 };
