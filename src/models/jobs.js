b7d55c2d0307f17c7e1372223b3494a4a1b70836
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2024-03-06 18:56:04 -0700

    Add jobs model
    Model that contains definitions for jobs listed in the Experience component.

---
+++ src/models/jobs.js
@@ -0,0 +1,32 @@
+const jobs = [
+    {
+        title: "Software Developer Intern",
+        company: "Progressive Insurance",
+        date: "May 2023 - August 2023",
+        description: [
+            "Designed and developed an Angular web application for a $1.5 billion subset of customers that saves approximately 302 hours of labor each year.",
+            "Developed and maintained a suite of automated regression tests for .NET web applications, leveraging C#, Selenium, and SpecFlow to decrease overall testing time and improve product quality."
+        ]
+    },
+    {
+        title: "Web Developer",
+        company: "Colorado State University Honors Program",
+        date: "November 2021 - December 2023",
+        description: [
+            "Maintain the Honors Program website which is used daily by over 1,900 students and faculty.",
+            "Develop web applications using ASP.NET MVC and Microsoft SQL Server on the back end, and MojoPortal and Bootstrap on the front end.",
+            "Reduced user-reported incidents by 20% year-over-year by implementing new features and fixing defects."
+        ]
+    },
+    {
+        title: "Research Assistant",
+        company: "CSU Computer Science Department",
+        date: "February 2023 - May 2023",
+        description: [
+            "Researched security threats in Android health and fitness apps using machine learning and natural language processing tools.",
+            "Developed a Python program to detect if a given Android application uses unsafe permissions and poses a privacy threat to users."
+        ]
+    }
+];
+
+export default jobs;
