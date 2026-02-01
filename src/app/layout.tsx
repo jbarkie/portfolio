280eeb5ab0ec4399c562f3f62d2d06119ac1b1dd
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2026-01-31 21:59:02 -0700

    Remove unused fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,11 +6,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
-import {
-  Saira_Extra_Condensed,
-  Sofia_Sans_Extra_Condensed,
-  Mulish,
-} from "next/font/google";
+import { Sofia_Sans_Extra_Condensed } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "Joseph Barkie: Software Developer",
@@ -31,12 +27,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html
-      lang="en"
-      className={`${saira.variable} ${sofia.variable} ${mulish.variable}`}
-    >
+    <html lang="en" className={`${sofia.variable}`}>
       <body id="page-top">
-        <noscript>You need to enable JavaScript to run this app.</noscript>
         <div id="root"></div>
       </body>
     </html>
